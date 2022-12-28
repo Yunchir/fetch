89835252b67487dfb9d53be93a73ad0e4790257d
Morning <morning@admins-iMac.local>
2022-12-28 13:52:40 +0800

    bank

--- bankUser/frontend/app.js
+++ bankUser/frontend/app.js
@@ -1,4 +1,17 @@
-search.addEventListener("click", () => {
-    fetch("http://localhost:3333/api/cars") 
+
+    fetch("http://localhost:3333/api/users") 
     .then((res) => res.json())
-})
+    .then((data) => {
+        let users = document.querySelector(".users")
+        data.map((user) =>  {
+            users.innerHTML += `<div class="user">
+            <div class="name">Name: ${user.name}</div>
+            <div class="balance">Balance: ${user.balance}</div>
+          </div>`
+        })
+    })
+
+    
+    function balance (users) {
+        
+    }
