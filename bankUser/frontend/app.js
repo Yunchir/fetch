94cd3fdd74a4e3ea5bed440f2c2a7a8732a6c8e1
Morning <morning@admins-iMac.local>
2023-01-05 09:13:34 +0800

    fetch

--- bankUser/frontend/app.js
+++ bankUser/frontend/app.js
@@ -1,12 +1,15 @@
+// const { clear } = require("console");
+
+let users = document.querySelector(".users");
 fetch("http://localhost:3333/api/users")
   .then((res) => res.json())
   .then((data) => {
     // balance(data);
 
-    let users = document.querySelector(".users");
     data.map((user) => {
       users.innerHTML += `<div class="user">
             <div class="name">Name: ${user.name}</div>
+            <div class="surName">sureName: ${user.surName}</div>
             <div class="balance">Balance: ${user.balance}</div>
           </div>`;
     });
@@ -15,21 +18,48 @@ fetch("http://localhost:3333/api/users")
 let button = document.querySelector("button");
 let input = document.querySelector("input");
 button.addEventListener("click", () => {
+  users.innerHTML= ""
   fetch("http://localhost:3333/api/users")
     .then((res) => res.json())
     .then((data) => {
-      for (i = 0; i < 1; i++) {
         data.map((user) => {
-          if (user.balance_type) {
-            let interestRate =
-              input.value * (user.balance * 0.1) + user.balance;
-            console.log(interestRate);
-          } else {
-            let interestRates =
-              input.value * (user.balance * 0.01) + user.balance;
-            // console.log(interestRates);
-          }
-        });
-      }
-    });
+          let money = user.balance 
+            for (i = 0; i < input.value; i++) {
+              if(user.balance_type){
+                money = money * 0.1 + money;
+                console.log(money)
+              } else {
+                money = money * 0.01 + money;
+                console.log(money)
+              }
+            }
+              users.innerHTML += `<div class="user">
+            <div class="name">Name: ${user.name}</div>
+            <div class="surName">sureName: ${user.surName}</div>
+            <div class="balance">Balance: ${money}</div>
+          </div>`;
+      })
+  });
 });
+
+clear.removeEventListener("click", () => {
+  
+  fetch("http://localhost:3333/api/users")
+  .then((res) => res.json())
+  .then((data) => {
+      data.map((user) => {
+        users.innerHTML = ""
+       
+          
+            users.innerHTML += `<div class="user">
+          <div class="name">Name: ${user.name}</div>
+          <div class="surName">sureName: ${user.surName}</div>
+          <div class="balance">Balance: ${user.balance}</div>
+        </div>`;
+    })
+});
+
+});
+
+
+
